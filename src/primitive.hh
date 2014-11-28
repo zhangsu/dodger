e05847feedd4e0dd57dce064f98ac753353bec69
Su Zhang <me@zhang.su>
2014-11-27 21:55:32 -0500

    Fix comment

--- src/primitive.hh
+++ src/primitive.hh
@@ -4,10 +4,10 @@
 #include "material.hh"
 #include "scene_node.hh"
 
-// The base type for any node in a scene graph.
+// The base type for renderable node in a scene graph.
 class Primitive : public SceneNode {
   public:
-    // Gets the material for this terrain.
+    // Gets the material for this primitive.
     const Material& material() const;
 
   protected:
