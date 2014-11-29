1a5f31f6e5db79fbd2ebfbe32e006f9aa918629c
Su Zhang <me@zhang.su>
2014-11-28 23:08:52 -0500

    Fix texture origin to be lower left
    The image library has its images originated at the top left corner, so a
    conversion is needed.

--- src/texture.cc
+++ src/texture.cc
@@ -12,18 +12,25 @@ Texture::Texture(string filename) {
 
     bind();
 
-    Image texture_map;
-    texture_map.loadPng(filename);
+    Image map;
+    map.loadPng(filename);
 
-    // Convert double data to float data.
-    size_t len = texture_map.width() * texture_map.height()
-                 * texture_map.elements();
+    // Convert double data to float data and flip the image from top left
+    // origin to bottom left origin.
+    size_t len = map.width() * map.height() * map.elements();
     vector<float> data(len);
-    for (size_t i = 0; i < len; ++i)
-        data[i] = texture_map.data()[i];
+    for (int y = 0; y < map.height(); ++y) {
+        for (int x = 0; x < map.width(); ++x) {
+            int data_index = y * map.width() + x;
+            int map_index = (map.height() - y - 1) * map.width() + x;
+            data[data_index * 3] = map.data()[map_index * 3];
+            data[data_index * 3 + 1] = map.data()[map_index * 3 + 1];
+            data[data_index * 3 + 2] = map.data()[map_index * 3 + 2];
+        }
+    }
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_map.width(),
-                 texture_map.height(), 0, GL_RGB, GL_FLOAT, &data[0]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, map.width(),
+                 map.height(), 0, GL_RGB, GL_FLOAT, &data[0]);
     checkGlError();
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
