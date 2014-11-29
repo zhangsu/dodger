c7540e63c911b1446507b94c63a26bdee9fa7ceb
Su Zhang <me@zhang.su>
2014-11-29 16:25:15 -0500

    Reduce the intensity of Sun slightly

--- src/game.cc
+++ src/game.cc
@@ -39,7 +39,7 @@ Game::Game()
     updateCameraTrans();
 
     // Add a global sunlight.
-    Light* sun = new Light(vec3(0.2, 0.2, 0.2), vec3(0.1, 0, 0));
+    Light* sun = new Light(vec3(0.1, 0.1, 0.1), vec3(0.1, 0, 0));
     sun->translate(0, 100, 0);
     addLight(&scene_, sun);
 }
